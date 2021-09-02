43cc59928bef2924a57b575c9656f6a5f7a395b3
Catari02 <89741808+Catari02@users.noreply.github.com>
2021-09-02 17:42:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function setup() {
 
 function portada() {
     createSpan("Estás en index.html").parent("title");
-    createElement('p','Tu ánimo hoy será'+random(lorenIpsum)+', por lo que ').parent(loPrincipal);
+    createElement('p','Tu ánimo hoy será '+random(lorenIpsum)+', por lo que ').parent(loPrincipal);
     select("a:nth-child(1)").style("font-weight", "bold").style("color", "#000000");
 }
 
